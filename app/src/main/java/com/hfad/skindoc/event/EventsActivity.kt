c0a3d4d01bacbbaaf939554becfff460dca79aaa
Rudraksh2605 <rudrakshasinghvan@gmail.com>
2024-12-06 11:09:20 +0530

    final

--- app/src/main/java/com/hfad/skindoc/event/EventsActivity.kt
+++ app/src/main/java/com/hfad/skindoc/event/EventsActivity.kt
@@ -1,11 +1,18 @@
 package com.hfad.skindoc.event
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
 import com.hfad.skindoc.R
+import com.hfad.skindoc.article.ArticlesListActivity
+import com.hfad.skindoc.chatbot.ChatBotActivity
+import com.hfad.skindoc.home.Home
+import com.hfad.skindoc.home.Home.Companion.CAMERA_REQUEST_CODE
 
 class EventsActivity : AppCompatActivity() {
 
@@ -16,6 +23,7 @@ class EventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.events)
+        setupNavigation()
 
 
         val events: RecyclerView = findViewById(R.id.events)
@@ -54,5 +62,38 @@ class EventsActivity : AppCompatActivity() {
 
             }
         })
+    }
+
+    private fun setupNavigation() {
+        val btn_article = findViewById<ImageButton>(R.id.nav_article)
+        val btn_home = findViewById<ImageButton>(R.id.nav_home)
+        val btn_scanner = findViewById<ImageButton>(R.id.nav_scanner)
+        val btn_event = findViewById<ImageButton>(R.id.nav_schedule)
+        val btn_chat_bot = findViewById<ImageButton>(R.id.nav_bot)
+
+        btn_article.setOnClickListener {
+            val intent = Intent(this, ArticlesListActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_scanner.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        }
+
+        btn_event.setOnClickListener {
+            val intent = Intent(this, EventsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_chat_bot.setOnClickListener {
+            val intent = Intent(this, ChatBotActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
